bf229770a8366498424f7fb2c2c21d0e7af2df97
dreamahui <dreamahui@163.com>
2015-08-11 16:02:57 +0800

    新增
    这是个测试

--- Test_Bluetool_1_Demo/ViewController.m
+++ Test_Bluetool_1_Demo/ViewController.m
@@ -8,7 +8,23 @@
 
 #import "ViewController.h"
 
-@interface ViewController ()
+@interface ViewController ()<CBCentralManagerDelegate, CBPeripheralDelegate>
+{
+    UIButton * ljButton;
+    BOOL        _cbReady;
+}
+
+@property(nonatomic) float batteryValue;
+
+@property (nonatomic, strong) CBCentralManager *centralManager;
+@property (nonatomic, strong) CBPeripheral *peripheral;
+
+@property (nonatomic, strong) NSMutableArray *peripherals;
+@property (strong,nonatomic) NSMutableArray *nDevices;
+@property (strong,nonatomic) NSMutableArray *nServices;
+@property (strong,nonatomic) NSMutableArray *nCharacteristics;
+
+@property (strong ,nonatomic) CBCharacteristic *writeCharacteristic;
 
 @end
 
@@ -17,7 +33,216 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    self.view.backgroundColor = [UIColor brownColor];
+    
+    self.centralManager = [[CBCentralManager alloc] initWithDelegate:self queue:nil];
+    self.centralManager.delegate = self;
+//    self.peripheral = [[CBPeripheral alloc] init];
+    
+    
+    self.peripherals = [NSMutableArray array];
+    
+    _nDevices = [[NSMutableArray alloc]init];
+    _nServices = [[NSMutableArray alloc]init];
+    _nCharacteristics = [[NSMutableArray alloc]init];
+    _cbReady = false;
+    //添加UI
+    [self initUI];
 }
+
+
+#pragma mark - CBCentralManagerDelegate
+-(void)centralManagerDidUpdateState:(CBCentralManager *)central
+{
+    switch (central.state) {
+        case CBCentralManagerStatePoweredOn:
+            NSLog(@"蓝牙已打开,请扫描外设");
+            break;
+            
+        default:
+            break;
+    }
+}
+-(void)centralManager:(CBCentralManager *)central didDiscoverPeripheral:(CBPeripheral *)peripheral advertisementData:(NSDictionary *)advertisementData RSSI:(NSNumber *)RSSI
+{
+
+    NSLog(@"已发现 peripheral: %@ rssi: %@, UUID: %@ advertisementData: %@ ", peripheral, RSSI, peripheral.UUID, advertisementData);
+    //
+    _peripheral=peripheral;
+    BOOL replace = NO;
+    // Match if we have this device from before
+    for (int i=0; i < _nDevices.count; i++) {
+        CBPeripheral *p = [_nDevices objectAtIndex:i];
+        if ([p isEqual:peripheral]) {
+            [_nDevices replaceObjectAtIndex:i withObject:peripheral];
+            replace = YES;
+        }
+    }
+    if (!replace) {
+        [_nDevices addObject:peripheral];
+//        [_deviceTable reloadData];
+    }
+}
+
+//连接外设成功，开始发现服务
+- (void)centralManager:(CBCentralManager *)central didConnectPeripheral:(CBPeripheral *)peripheral {
+    NSLog(@"成功连接 peripheral: %@ with UUID: %@",peripheral,peripheral.UUID);
+    
+    [self.peripheral setDelegate:self];
+    [self.peripheral discoverServices:nil];
+    NSLog(@"扫描服务");
+}
+//连接外设失败
+-(void)centralManager:(CBCentralManager *)central didFailToConnectPeripheral:(CBPeripheral *)peripheral error:(NSError *)error
+{
+    NSLog(@"%@",error);
+}
+
+#pragma mark - CBPeripheralDelegate
+-(void)peripheralDidUpdateRSSI:(CBPeripheral *)peripheral error:(NSError *)error
+{
+    NSLog(@"%s,%@",__PRETTY_FUNCTION__,peripheral);
+    int rssi = abs([peripheral.RSSI intValue]);
+    CGFloat ci = (rssi - 49) / (10 * 4.);
+    NSString *length = [NSString stringWithFormat:@"发现BLT4.0热点:%@,距离:%.1fm",_peripheral,pow(10,ci)];
+    NSLog(@"距离：%@",length);
+}
+//已发现服务
+-(void) peripheral:(CBPeripheral *)peripheral didDiscoverServices:(NSError *)error
+{
+    NSLog(@"发现服务.");
+    int i=0;
+    for (CBService *s in peripheral.services) {
+        [self.nServices addObject:s];
+    }
+    for (CBService *s in peripheral.services) {
+        NSLog(@"%d :服务 UUID: %@(%@)",i,s.UUID.data,s.UUID);
+        i++;
+        [peripheral discoverCharacteristics:nil forService:s];
+    }
+}
+
+//已搜索到Characteristics
+-(void) peripheral:(CBPeripheral *)peripheral didDiscoverCharacteristicsForService:(CBService *)service error:(NSError *)error
+{
+    NSLog(@"发现特征的服务:%@ (%@)",service.UUID.data ,service.UUID);
+    for (CBCharacteristic *c in service.characteristics) {
+        NSLog(@"特征 UUID: %@ (%@)",c.UUID.data,c.UUID);
+        if ([c.UUID isEqual:[CBUUID UUIDWithString:@"2A06"]]) {
+            _writeCharacteristic = c;
+        }
+        if ([c.UUID isEqual:[CBUUID UUIDWithString:@"2A19"]]) {
+            [_peripheral readValueForCharacteristic:c];
+        }
+        
+        if ([c.UUID isEqual:[CBUUID UUIDWithString:@"FFA1"]]) {
+            [_peripheral readRSSI];
+        }
+        [_nCharacteristics addObject:c];
+        
+        NSString *path = [[NSBundle mainBundle] pathForResource:@"Set_Left_Icon_Selected" ofType:@"png"];
+        
+        [peripheral writeValue:[NSData dataWithContentsOfFile:path] forCharacteristic:c type:CBCharacteristicWriteWithResponse];
+    }
+}
+
+
+//获取外设发来的数据，不论是read和notify,获取数据都是从这个方法中读取。
+- (void)peripheral:(CBPeripheral *)peripheral didUpdateValueForCharacteristic:(CBCharacteristic *)characteristic error:(NSError *)error
+{
+    // BOOL isSaveSuccess;
+    
+    if ([characteristic.UUID isEqual:[CBUUID UUIDWithString:@"2A19"]]) {
+        NSString *value = [[NSString alloc]initWithData:characteristic.value encoding:NSUTF8StringEncoding];
+        _batteryValue = [value floatValue];
+        NSLog(@"电量%f",_batteryValue);
+    }
+    if ([characteristic.UUID isEqual:[CBUUID UUIDWithString:@"FFA1"]]) {
+        NSString *value = [[NSString alloc]initWithData:characteristic.value encoding:NSUTF8StringEncoding];
+        //_batteryValue = [value floatValue];
+        NSLog(@"信号%@",value);
+    }
+    
+    else
+        NSLog(@"didUpdateValueForCharacteristic%@",[[NSString alloc] initWithData:characteristic.value encoding:NSUTF8StringEncoding]);
+}
+//中心读取外设实时数据
+- (void)peripheral:(CBPeripheral *)peripheral didUpdateNotificationStateForCharacteristic:(CBCharacteristic *)characteristic error:(NSError *)error {
+    if (error) {
+        NSLog(@"Error changing notification state: %@", error.localizedDescription);
+    }
+    // Notification has started
+    if (characteristic.isNotifying) {
+        [peripheral readValueForCharacteristic:characteristic];
+        
+    } else { // Notification has stopped
+        // so disconnect from the peripheral
+        NSLog(@"Notification stopped on %@.  Disconnecting", characteristic);
+        [self.centralManager cancelPeripheralConnection:self.peripheral];
+    }
+}
+//用于检测中心向外设写数据是否成功
+-(void)peripheral:(CBPeripheral *)peripheral didWriteValueForCharacteristic:(CBCharacteristic *)characteristic error:(NSError *)error
+{
+    if (error) {
+        NSLog(@"=======%@",error.userInfo);
+    }else{
+        NSLog(@"发送数据成功");
+    }
+}
+
+
+
+#pragma mark - 扫描设备
+- (void)scanClick
+{
+    NSLog(@"扫描超时,停止扫描");
+    [self.centralManager scanForPeripheralsWithServices:nil options:@{CBCentralManagerScanOptionAllowDuplicatesKey : @YES }];
+    
+    double delayInSeconds = 10.0;
+    dispatch_time_t popTime = dispatch_time(DISPATCH_TIME_NOW, (int64_t)(delayInSeconds * NSEC_PER_SEC));
+    dispatch_after(popTime, dispatch_get_main_queue(), ^(void){
+        [_centralManager stopScan];
+        NSLog(@"30秒,扫描超时,停止扫描");
+    });
+}
+
+#pragma mark - 连接设备
+-(void)connectClick:(id)sender
+{
+    if (_cbReady ==false) {
+        [self.centralManager connectPeripheral:_peripheral options:nil];
+        _cbReady = true;
+        [ljButton setTitle:@"断开" forState:UIControlStateNormal];
+    }else {
+        [self.centralManager cancelPeripheralConnection:_peripheral];
+        _cbReady = false;
+        [ljButton setTitle:@"连接" forState:UIControlStateNormal];
+    }
+}
+
+
+#pragma mark - 设置UI视图
+- (void)initUI
+{
+
+    UIButton * smButton = [UIButton buttonWithType:UIButtonTypeCustom];
+    [smButton setTitle:@"扫描" forState:UIControlStateNormal];
+    smButton.frame = CGRectMake(20, 100, 60, 30);
+    [smButton addTarget:self action:@selector(scanClick) forControlEvents:UIControlEventTouchUpInside];
+    [self.view addSubview:smButton];
+    
+    ljButton = [UIButton buttonWithType:UIButtonTypeRoundedRect];
+    [ljButton setTitle:@"连接" forState:UIControlStateNormal];
+    ljButton.frame = CGRectMake(90, 100, 60, 30);
+    [ljButton addTarget:self action:@selector(connectClick:) forControlEvents:UIControlEventTouchUpInside];
+    [self.view addSubview:ljButton];
+}
+
+
+
+
+
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
